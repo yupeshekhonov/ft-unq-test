d22d6247b5bce32444ba65bc817d67cc189ab441
Yuriy Peshekhonov <yupeshekhonov@usetech.com>
2022-12-14 15:22:09 +0300

    + solidity

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,18 @@
 import {Sdk} from '@unique-nft/sdk'
+import {ethers} from 'hardhat'
 import {KeyringProvider} from '@unique-nft/accounts/keyring'
+import {
+  UniqueFungibleFactory,
+  UniqueFungible,
+  CollectionHelpersFactory,
+} from '@unique-nft/solidity-interfaces'
+import {Address} from '@unique-nft/utils/address'
 
 const baseUrl = 'https://rest.unique.network/opal/v1'
-// dummy account created only for this test 
+// dummy account created only for this test
 const mnemonic = 'solar float siege poverty catch resource uncle verify kitchen cactus frog skill'
 const substrateMirror = '5DVgiNh1Go8ESa18xEirA4uV3zpra59awPGQjJTmCrqYMNtM'
+const myEthAddress = '0xb4d6A98aa8CD5396069c2818Adf4ae1A0384b43a'
 
 async function main() {
   const account = await KeyringProvider.fromMnemonic(mnemonic)
@@ -31,7 +39,7 @@ async function main() {
   }
   const collectionId = collectionResult.parsed?.collectionId
 
-  const tokenResult = await sdk.fungible.addTokens.submitWaitResult({
+  /* const tokenResult = await sdk.fungible.addTokens.submitWaitResult({
     address,
     // @ts-ignore
     collectionId,
@@ -47,7 +55,7 @@ async function main() {
       // amount - undefined
       `The ${tokenResult.parsed?.amount} tokens were created in collection # ${tokenResult.parsed?.collectionId}`
     )
-  }
+  } */
 
   const transferResult = await sdk.collections.transfer.submitWaitResult({
     // @ts-ignore
@@ -59,6 +67,25 @@ async function main() {
   console.log(
     `Collection # ${transferResult.parsed?.collectionId} was transferred to the ${transferResult.parsed?.owner} address.`
   )
+
+  // @ts-ignore
+  const collectionAddress = Address.collection.idToAddress(collectionId)
+
+  const provider = ethers.provider
+  const privateKey = process.env.PRIVATE_KEY
+  // @ts-ignore
+  const wallet = new ethers.Wallet(privateKey, provider)
+
+  // @ts-ignore
+  const collectionHelpers = await CollectionHelpersFactory(wallet, ethers)
+
+  const collection = await UniqueFungibleFactory(collectionAddress, wallet, ethers)
+
+  const txMintToken = await (await collection.mint(wallet.address, 50)).wait()
+	console.log(txMintToken)
 }
 
-main()
+main().catch((err) => {
+    console.error(err);
+    process.exitCode = 1;
+});
